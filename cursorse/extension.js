eb50f317783e78df676f2de1eba1341ed5d3f614
userfriendanonymous <106442915+userfriendanonymous@users.noreply.github.com>
2022-07-09 08:08:10 +0600

    +cursor se

---
+++ cursorse/extension.js
@@ -0,0 +1,35 @@
+class cursor {
+    constructor() { }
+    getInfo() {
+        return {
+            id: 'cursor',
+            name: 'Cursor',
+            color1: '#8BC34A',
+            color2: '#7CB342',
+            color3: '#689F38',
+            blocks: [
+                {
+                    opcode: 'cursormode',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'cursor mode [mode]',
+                    arguments: {
+                        mode: {
+                            type: Scratch.ArgumentType.STRING,
+                            menu: 'mode',
+                            defaultValue: 'pointer'
+                        }
+                    }
+                }
+            ],
+            menus: {
+                mode: {
+                    items: ['pointer', 'normal']
+                }
+            }
+        };
+    }
+    cursormode(mode) {
+        document.body.style.cursor = mode;
+    }
+}
+Scratch.extensions.register(new cursor());
